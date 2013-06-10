326cf470b35b187f946699999518f27492b190c2
Ivan Muller <mullerivan@gmail.com>
2013-06-09 22:22:09 -0300

    scopes

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -3,4 +3,7 @@ class Debt < ActiveRecord::Base
   belongs_to :creditor, :class_name => "Person"
   belongs_to :food
   attr_accessible :name, :quantity
+
+  scope :debtors, lambda { |person_id| where("debtor_id = ?", person_id ) }
+  scope :creditors, lambda { |person_id| where("creditor_id = ?", person_id ) }
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,4 @@
 class Person < ActiveRecord::Base
   attr_accessible :name
   has_many :amounts
-  has_many :debts
 end
